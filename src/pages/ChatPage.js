154ca1eb6d3227c9a616d590bfdb9bd131bdb6de
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2025-04-26 15:47:55 -0700

    📝 Add StudentsPage and ChatPage placeholder pages

---
+++ src/pages/ChatPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function ChatPage() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Chat
+      </Typography>
+      <Typography>
+        🚧 Chat interface will go here.
+      </Typography>
+    </div>
+  );
+}
---
+++ src/pages/StudentsPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+export default function StudentsPage() {
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom>
+        Students
+      </Typography>
+      <Typography>
+        🚧 Student Data Table will go here.
+      </Typography>
+    </div>
+  );
+}
